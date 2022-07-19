da59982bf9ba39723caaee81a55982f193cd25b4
zgd <zgd@gd.com>
2022-07-19 14:34:46 +0800

    ok

---
+++ src/main/java/com/zgd/leetcode/editor/cn/MoveZeroes.java
@@ -0,0 +1,77 @@
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。 
+//
+// 请注意 ，必须在不复制数组的情况下原地对数组进行操作。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [0,1,0,3,12]
+//输出: [1,3,12,0,0]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: nums = [0]
+//输出: [0] 
+//
+// 
+//
+// 提示: 
+// 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -2³¹ <= nums[i] <= 2³¹ - 1 
+// 
+//
+// 
+//
+// 进阶：你能尽量减少完成的操作次数吗？ 
+// Related Topics 数组 双指针 👍 1661 👎 0
+
+
+  
+package com.zgd.leetcode.editor.cn;
+
+public class MoveZeroes{
+
+  /**
+  * 283
+  * 移动零
+  * 
+  * 
+  *
+  * 2022-07-19 11:46:45
+  */  
+  public static void main(String[] args) {
+    Solution solution = new MoveZeroes().new Solution();
+  }
+  
+  //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void moveZeroes(int[] nums) {
+      //思路 从第0位开始,如果是0,就往后面找,找到不是0的 两两替换
+      for (int i = 0; i < nums.length-1; i++) {
+        if (nums[i] == 0){
+          int j ;
+          for (j = i+1; j < nums.length; j++) {
+            if (nums[j]!=0){
+              break;
+            }
+          }
+          if (j == nums.length ){
+            continue;
+          }
+          int t = nums[i];
+          nums[i] = nums[j];
+          nums[j] = t;
+        }
+      }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
