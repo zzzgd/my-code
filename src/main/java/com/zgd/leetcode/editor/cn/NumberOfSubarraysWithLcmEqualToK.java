f729c59ff5d0f3bc8632354f99c142d143aff9f6
zgd <zgd@gd.com>
2023-06-14 15:50:35 +0800

    m

--- src/main/java/com/zgd/leetcode/editor/cn/NumberOfSubarraysWithLcmEqualToK.java
+++ src/main/java/com/zgd/leetcode/editor/cn/NumberOfSubarraysWithLcmEqualToK.java
@@ -54,75 +54,61 @@ public class NumberOfSubarraysWithLcmEqualToK{
   */  
   public static void main(String[] args) {
     Solution solution = new NumberOfSubarraysWithLcmEqualToK().new Solution();
-    solution.subarrayLCM(new int[]{2,10,4,7,5,10,5},20);
+    solution.subarrayLCM(new int[]{3,6,2,7,1},6);
   }
   
   //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int subarrayLCM(int[] nums, int k) {
-        //首先第一步,找到数组中能被k整除的且不等于1的,连续的
-        List<List<Integer>> chushulist = new ArrayList<>();
-        List<Integer> chushu = new ArrayList<>();
+        //首先第一步,找到数组中能被k整除的且不等于1的,连续的放到list
+        List<List<Integer>> llist = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         for (int num : nums) {
             //如果k能被num整除,且num不等于1, 或者k是
             if(k % num == 0 ){
-                chushu.add(k/num);
+                list.add(num);
             }else{
-                if(!chushu.isEmpty()){
-                    chushulist.add(new ArrayList<>(chushu));
-                    chushu.clear();
+                if(!list.isEmpty()){
+                    llist.add(new ArrayList<>(list));
+                    list.clear();
                 }
             }
         }
-        if(!chushu.isEmpty()) {
-            chushulist.add(new ArrayList<>(chushu));
+        if(!list.isEmpty()) {
+            llist.add(new ArrayList<>(list));
         }
 
-        //第二步,从下标0开始,找到一个区间, 这个区间内要么有个数的除数是1, 要么这些除数互相不能被全部整除
+        //第二步,从左到右,把数两两合并,得到他们的最小公倍数,比如 2,10,4,5. 2和10合并最小公倍数是10,接着10和4,最小公倍数是20
         int n = 0;
-        for (List<Integer> chushuarr : chushulist) {
-            for (int i = 0; i < chushuarr.size(); i++) {
-                for (int j = i; j < chushuarr.size(); j++) {
-                    if(hasGongBei(i,j,chushuarr)){
-                        //i~j之间找到了最小公倍数的组合,以及i~j以后每加一个元素都算一个,j后面都不用看了
-                        n+=(chushuarr.size()-j);
+        for (List<Integer> arr : llist) {
+            for (int i = 0; i < arr.size(); i++) {
+                int gbs = arr.get(i);
+                for (int j = i; j < arr.size(); j++) {
+                    //两两求最小公倍数来合并
+                    gbs = gongBeiShu(gbs,arr.get(j));
+                    if(gbs == k){
+                        //一旦找到符合的,后面的都符合,数量有几个就加几个
+                        n += arr.size()-j;
+                        break;
+                    }else if(gbs > k){
+                        //超出了预期,后面都没有了
                         break;
                     }
                 }
             }
         }
+
         return n;
     }
 
       /**
-       * 计算i~j之间是否为最小公倍数的要素就是, i~j之间的除数,注意是除数, 两两相除,是否有不能被整除的. 比如 2,4,10 和 20, 除数是10,5,2, 其中除数5和2不能整除,所以这组是最小公倍数
-       * 同理, 如果是2,4,10和40, 除数是20,10,4, 除数互相能被整除,则不是一组最小公倍数
-       * @param i
-       * @param j
-       * @param chushulist
+       * 求两者之间的最小公倍数, 就是 a*b/最大公约数(a,b)
+       * @param a
+       * @param b
        * @return
        */
-    private boolean hasGongBei(int i,int j,List<Integer> chushulist){
-        //如果除数为1,则为自身
-        if(  chushulist.get(j) == 1 )return true;
-        //判断j和前面i~j之间的元素的除数 是否有非1的最大公约数,比如 4,5,20和100,除数是25和20,5,有共同的最大公约数5,则这组也就不是最小公倍数
-        Integer jchu = chushulist.get(j);
-        //判断他们是不是有共同的最大公约数
-        int lastGongyue = -1;
-        for (int k = i; k < j; k++) {
-            Integer kchu = chushulist.get(k);
-            if(kchu == jchu){
-                continue;
-            }
-            int n = gongYueShu(jchu, kchu);
-            //各个值有不同的最大公约数,或者都是1,才满足条件,相反不满足,比如2,6,4,10,8, 都是2. 但是要剔除相等的部分
-            if(lastGongyue != -1 && n != lastGongyue){
-                //只要有一对不为公约数,这组就可以是最小公倍数
-                return true;
-            }
-            lastGongyue = n;
-        }
-        return lastGongyue == 1;
+    private int gongBeiShu(int a,int b){
+        return a*b/gongYueShu(a,b);
     }
 
     //最对大公约数, 辗转相除法, gcd(A, B) = gcd(B, A mod B)   其中:A > B
